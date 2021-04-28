181f099596ef5b2c5aae02c58d5ea30a32fd2736
Jakob Persons <jakobep24@gmail.com>
2021-04-27 22:42:13 -0400

    header, particles, typed text completed

--- src/App.js
+++ src/App.js
@@ -2,11 +2,33 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from './components/Navbar';
 import Header from './components/Header';
+import Particles from 'react-particles-js';
 
 
 function App() {
   return (
     <div className="App">
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+                value_area: 900
+              }
+            },
+            shape: {
+              type: "line",
+              stroke: {
+                width: 10,
+                color: "#f9ab00"
+              }
+            }
+          }
+        }} 
+      />
       <Navbar />
       <Header />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import Typed from 'react-typed';
 
 const Header = () => {
    return (
       <div className="header-wrapper">
          <div className="main-info">
-            <h1>Web development and website promotions</h1>
+            <h1>Software Engineer</h1>
+            <h1>Full Stack Web Developer</h1>
+            <Typed
+               className="typed-text"
+               strings={[
+                  "JavaScript",
+                  "React",
+                  "Ruby on Rails",
+                  "Git",
+                  "PostgreSQL",
+                  "HTML",
+                  "CSS",
+                  "Python",
+                  "BootStrap"
+               ]} 
+               typeSpeed={40}
+               backSpeed={60}
+               loop
+            />
+            <a href="#" className="btn-main-offer">Contact</a>
          </div>
       </div>
    )
