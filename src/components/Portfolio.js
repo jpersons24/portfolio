0a897b0144ed3b5067a5e96dd7a0a9d649600ab3
Jakob Persons <jakobep24@gmail.com>
2021-04-28 21:47:30 -0400

    portfolio search icon added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,6 +3,9 @@ import netflix from '../images/netflix.png';
 import cityGuide from '../images/city-guide-app.png';
 import portfolio from '../images/portfolio.png';
 import taskManager from '../images/task-manager.png';
+// FONT AWESOME IMPORTS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearchPlus } from '@fortawesome/free-solid-svg-icons'
 
 
 
@@ -12,19 +15,29 @@ const Portfolio = () => {
          <div className="container">
             <h1 className="text-uppercase text-center py-5">Portfolio</h1>
             <div className="image-box-wrapper row justify-content-center">
-               <img className="portfolio-image" src={netflix} alt="Netflix Clone..." />
-            </div>
+               <div className="portfolio-image-box">
+                  <img className="portfolio-image" src={netflix} alt="Netflix Clone..." />
+                  <div className="overflow"></div>
+                  <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+               </div>
             {/*  -  */}
-            <div className="image-box-wrapper row justify-content-center">
-               <img className="portfolio-image" src={cityGuide} alt="City Guide project..." />
-            </div>
+               <div className="portfolio-image-box">
+                  <img className="portfolio-image" src={cityGuide} alt="City Guide project..." />
+                  <div className="overflow"></div>
+                  <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+               </div>
             {/*  -  */}
-            <div className="image-box-wrapper row justify-content-center">
-               <img className="portfolio-image" src={portfolio} alt="Portfolio project..." />
-            </div>
+               <div className="portfolio-image-box">
+                  <img className="portfolio-image" src={portfolio} alt="Portfolio project..." />
+                  <div className="overflow"></div>
+                  <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+               </div>
             {/*  -  */}
-            <div className="image-box-wrapper row justify-content-center">
-               <img className="portfolio-image" src={taskManager} alt="Task manager project..." />
+               <div className="portfolio-image-box">
+                  <img className="portfolio-image" src={taskManager} alt="Task manager project..." />
+                  <div className="overflow"></div>
+                  <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
+               </div>
             </div>
          </div>
       </div>
