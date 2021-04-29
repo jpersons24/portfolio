86866a114062ad647dd400e35a7701e4adb0578c
Jakob Persons <jakobep24@gmail.com>
2021-04-29 07:13:29 -0400

    first pop up box added

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,4 +1,5 @@
 import React from 'react';
+// IMAGE IMPORTS
 import netflix from '../images/netflix.png';
 import cityGuide from '../images/city-guide-app.png';
 import portfolio from '../images/portfolio.png';
@@ -6,16 +7,44 @@ import taskManager from '../images/task-manager.png';
 // FONT AWESOME IMPORTS
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSearchPlus } from '@fortawesome/free-solid-svg-icons'
+// REACT POP UP BOX
+import { PopupboxManager, PopupboxContainer } from 'react-popupbox';
+import 'react-popupbox/dist/react-popupbox.css';
 
 
 
 const Portfolio = () => {
+
+
+   const openNetflix = () => {
+      const content = (
+         <>
+            <img className="portfolio-image-popupbox" src={netflix} alt="Netflix Clone..." />
+            <p>Description of the project. Make sure to link the github repository below.</p>
+            <b>Github:</b> <a className="hyper-link" onClick={() => window.open("https://github.com/jpersons24/discourse")}>https://github.com/jpersons24/discourse</a>
+         </>
+      )
+      PopupboxManager.open({ content })
+   }
+
+   const popUpBoxConfigNetflix = {
+      titleBar: {
+         enable: true,
+         text: "Netflix Clone Project",
+      },
+      fadeIn: true,
+      fadeInSpeed: 500,
+   }
+
+
+
+
    return (
       <div className="portfolio-wrapper">
          <div className="container">
             <h1 className="text-uppercase text-center py-5">Portfolio</h1>
             <div className="image-box-wrapper row justify-content-center">
-               <div className="portfolio-image-box">
+               <div className="portfolio-image-box" onClick={openNetflix}>
                   <img className="portfolio-image" src={netflix} alt="Netflix Clone..." />
                   <div className="overflow"></div>
                   <FontAwesomeIcon className="portfolio-icon" icon={faSearchPlus} />
@@ -40,6 +69,7 @@ const Portfolio = () => {
                </div>
             </div>
          </div>
+         <PopupboxContainer {...popUpBoxConfigNetflix} />
       </div>
    )
 }
